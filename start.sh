0105f0c30f30f44d6117e126899476d5557efa54
yangmiao <529700260@qq.com>
2019-11-23 17:20:41 +0800

    1

---
+++ start.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+__run() {
+  echo -e "123456\n123456" | (passwd)
+  /usr/sbin/sshd -D
+}
+
+__start() {
+  #程序名
+  RUN_NAME="$1"
+  #jar 位置
+  JAVA_OPTS=/var/www/
+  source /etc/profile
+  bash $JAVA_OPTS/bin/stop.sh
+  bash $JAVA_OPTS/bin/start.sh
+  echo "$RUN_NAME started success."
+}
+
+# Call all functions
+__start $1
+__run
